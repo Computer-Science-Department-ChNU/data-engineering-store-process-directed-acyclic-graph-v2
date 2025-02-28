2ca5807d8188d19b13725ebab36ac39ec5e96b89
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2025-02-28 14:46:52 +0200

    [0] Initial comit.

---
+++ src/main/kotlin/ua/edu/chnu/kkn/dag/DagApplication.kt
@@ -0,0 +1,11 @@
+package ua.edu.chnu.kkn.dag
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DagApplication
+
+fun main(args: Array<String>) {
+	runApplication<DagApplication>(*args)
+}
